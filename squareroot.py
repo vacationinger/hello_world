746dd64d185463d306d9c2db42abe39334169fe9
vacationinger <mv703449@cii.shizuoka.ac.jp>
2019-10-08 14:57:08 +0900

    change_param exercise second step

--- squareroot.py
+++ squareroot.py
@@ -1,7 +1,7 @@
 # Python Program to calculate the square root
 
 # Note: change this value for a different result
-num = 8 
+num = 16 
 
 num_sqrt = num ** 0.5
 print('The square root of {0:0.3f} is {1:0.3f}'.format(num, num_sqrt))
